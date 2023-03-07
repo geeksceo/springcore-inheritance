5dd14c7b084587a674db81fd085d30e3bb8ad87b
geeksceo <oowashina@gmail.com>
2023-03-07 14:51:28 +0000

    Dare mighty things.

---
+++ src/main/java/com/skan/inheritance/SpringcoreInheritanceApplication.java
@@ -0,0 +1,13 @@
+package com.skan.inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreInheritanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skan/inheritance/SpringcoreInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skan.inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
